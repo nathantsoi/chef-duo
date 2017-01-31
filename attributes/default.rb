53b7e4ea795d7c9717db853288944f89a491c4f2
nathan <nathan@vertile.com>
2017-01-30 16:57:42 -0800

    set pushinfo and autopush via attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,6 +10,9 @@ default['duo']['tarball_version'] = 'duo_unix-1.9.14.tar.gz'
 default['duo']['git_repo'] = 'https://github.com/duosecurity/duo_unix.git'
 default['duo']['git_rev'] = 'master'
 
+default['duo']['pushinfo'] = 'yes'
+default['duo']['autopush'] = 'yes'
+
 # sshd, note that	key-words are case-insensitive and arguments are case-sensitive, however overrides are case sensitive
 # the silly _p_a_m will yield _PAM
 default['openssh']['server']['use_p_a_m'] = 'yes'
